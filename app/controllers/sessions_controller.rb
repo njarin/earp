b0ca326cb43e5c15b24a459b2ef8d27871895673
Eitan Greene <email": null>
2017-04-20 11:49:17 -0700

    Namespaces params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    p login_params
+  end
+
+  private
+  def login_params
+    params.require(:session).permit(:email, :password)
+  end
 end
