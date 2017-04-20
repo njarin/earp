9197dc0f2a10a14eb0d565065996c5d3128ed6da
Eitan Greene & Seth Gerou <pair+eitan-g.sethgerou@devbootcamp.com>
2017-04-19 19:39:09 -0700

    Creates action for experiment#new

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,5 +1,22 @@
 class ExperimentsController < ApplicationController
-  def new
+  def show
 
+  end
+
+  def new
+  end
+
+  def create
+    @experiment = Experiment.new(experiment_params)
+    if @experiment.save
+      redirect_to show_path(@experiment)
+    else
+      render :new
+    end
+  end
+
+  private
+  def experiment_params
+    params.require(:experiments).permit(:title, :summary, :hypothesis, :observations, :results, :contact_info, :staff_needed)
   end
 end
