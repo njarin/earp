10f374f708fb3ccebbc54ceceafb007a110444a5
Eitan Greene <email": null>
2017-04-20 12:16:27 -0700

    Authenticates user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    p login_params
+    user = User.find_by(email: login_params[:email])
+    if user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      redirect_to experiments_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
   private
