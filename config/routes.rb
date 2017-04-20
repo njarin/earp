e2cd96c26cff663b32188bd5442a005108626aa2
Dan McKeon & Julia(e) Xu <pair+danmckeon.jmxu94@devbootcamp.com>
2017-04-20 12:41:56 -0700

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :experiments, only: [:index, :show, :new, :create, :update]
+  resources :users, only: [:new, :create]
 
 end
