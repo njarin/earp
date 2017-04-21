13d468b83dc9f6dd571eb32b2aaab96b159c257c
Nick Jarin <email": null>
2017-04-20 19:43:30 -0700

    Add capybara tests for registration and pass them both

--- spec/features/registering_a_user_spec.rb
+++ spec/features/registering_a_user_spec.rb
@@ -4,18 +4,33 @@ feature "registering a user", js: true do
   context "successful registration" do
     scenario "visitor registers for EARP" do
       visit '/users/new'
+
       click_on("Register")
       within(".new_user") do
-        fill_in "#user_name", with: "Brianna"
-        fill_in "#user_email", with: "bribri@bmail.com"
-        fill_in "#user_password", with: "123"
-        find("#role").find(:xpath, 'option[2]').select_option
+        fill_in "user[name]", with: "Brianna"
+        fill_in "user[email]", with: Faker::Internet.free_email
+        fill_in "user[password]", with: "123"
+        select "EA staff", from: 'user[role]'
+        click_on "Register"
       end
-      p "YEAH WE DID IT"
+      sleep(3)
+      expect(page).to have_content "Experiment Board"
+      sleep(3)
     end
   end
 
   context "unsuccessful registration" do
-
+    scenario "visitor unsuccessfully registers for EARP" do
+      visit '/users/new'
+      click_on("Register")
+      within(".new_user") do
+        fill_in "user[name]", with: "Brianna"
+        fill_in "user[email]", with: ""
+        fill_in "user[password]", with: "123"
+        select "EA staff", from: 'user[role]'
+        click_on "Register"
+      end
+      expect(page).to have_content "You must include a valid name, email, and password"
+    end
   end
 end
