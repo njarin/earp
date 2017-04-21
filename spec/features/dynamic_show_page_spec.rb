6105f585790ea7b265ef52a23d710d40953a210f
Julia(e) Xu <email": null>
2017-04-20 21:13:28 -0700

    SAVE POINT: capybara database issue

---
+++ spec/features/dynamic_show_page_spec.rb
@@ -0,0 +1,27 @@
+# require "rails_helper"
+#TABLED FOR LATER
+
+# feature "reveal more info on experiment", js: true do
+#   before(:each) do
+#     visit root_path
+#     fill_in "session_email", with: "faculty@example.com"
+#     fill_in "session_password", with: "password"
+#     find("input[name='commit']").click
+#     sleep(3)
+#   end
+
+#   scenario "reveals project info" do
+#     visit experiment_path(1)
+#     click_button('reveal-info')
+#     sleep(3)
+#   end
+# end
+
+# feature "hide more info on experiment" do
+#   context "clicking the hide button" do
+#     scenario "hides project info" do
+
+#     end
+
+#   end
+# end
