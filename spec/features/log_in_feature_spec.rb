4b00970bd0cb5dfb0a03bc3cb5b966cc4a7c1f59
Dan McKeon & Julia(e) Xu <pair+danmckeon.jmxu94@devbootcamp.com>
2017-04-20 19:23:47 -0700

    login + logout feature tests

--- spec/features/log_in_feature_spec.rb
+++ spec/features/log_in_feature_spec.rb
@@ -18,6 +18,20 @@ feature 'login', js: true do
     fill_in 'session_password', with: 'elephants are large'
 
     find('input[name="commit"]').click
-    expect(current_path).to eq '/sessions'
+    expect(current_path).to eq sessions_path
+  end
+end
+
+feature 'logout', js: true do
+  it 'Logged in user can log out' do
+    visit root_path
+
+    fill_in 'session_email', with: 'staff@example.com'
+    fill_in 'session_password', with: 'password'
+
+    find('input[name="commit"]').click
+    click_link('Logout')
+    expect(current_path).to eq root_path
+
   end
 end
